2b7876d72c8185f8d83a670901473b9da2ea07f1
Jana5642 <jancaurbanova@seznam.cz>
2023-04-17 09:12:41 +0200

    úkol

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -8,6 +8,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Aplikace extends JFrame {
+    private JLabel husyLabel;
+    private JLabel kraliciLabel;
+    private JTextField husyField;
+    private JTextField kraliciField;
+    private JButton vypocitatButton;
+    private JLabel pocetHlavLabel;
+    private JLabel pocetNohouLabel;
+    private JTextField pocetHlavField;
+    private JTextField pocetNohouField;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
@@ -24,6 +33,7 @@ public class Aplikace extends JFrame {
         setVisible(true);
     }
 
+
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setIconImage(new ImageIcon(Aplikace.class.getResource("czechitas-icon.png")).getImage());
@@ -31,8 +41,55 @@ public class Aplikace extends JFrame {
         setMinimumSize(new Dimension(250, 200));
 
         //TODO implementovat formulář podle zadání
+        //Bohužel si nevím rady s převodem ze string na int, to stejné se zarovnáním tlačítka "vypočítej" na střed řádku.
+
+        husyField = new JTextField();
+        husyLabel = new JLabel("Husy");
+        husyLabel.setEnabled(true);
+        husyLabel.setLabelFor(husyField);
+        husyField.setHorizontalAlignment(JTextField.TRAILING);
+        husyLabel.setDisplayedMnemonic('H');
+        add(husyLabel);
+        add(husyField);
+
+        kraliciField = new JTextField();
+        kraliciLabel = new JLabel("Králíci");
+        kraliciLabel.setEnabled(true);
+        kraliciLabel.setLabelFor(kraliciField);
+        kraliciField.setHorizontalAlignment(JTextField.TRAILING);
+        add(kraliciLabel);
+        add(kraliciField);
+
+        add(createButtonBar(),"span");
+
+        pocetHlavField = new JTextField();
+        pocetHlavLabel = new JLabel("Počet hlav");
+        pocetHlavField.setEditable(false);
+        pocetHlavLabel.setLabelFor(pocetHlavField);
+        pocetHlavField.setHorizontalAlignment(JTextField.TRAILING);
+        pocetHlavLabel.setDisplayedMnemonic(0);
+        add(pocetHlavLabel);
+        add(pocetHlavField);
+
+        pocetNohouField = new JTextField();
+        pocetNohouLabel = new JLabel("Počet nohou");
+        pocetNohouField.setEditable(false);
+        pocetNohouLabel.setLabelFor(pocetNohouField);
+        pocetNohouField.setHorizontalAlignment(JTextField.TRAILING);
+        add(pocetNohouLabel);
+        add(pocetNohouField);
 
         pack();
     }
+    private JPanel createButtonBar() {
+        vypocitatButton = new JButton("Vypočítat");
+        vypocitatButton.setMnemonic('V');
+
+
+        JPanel buttonBar = new JPanel(new MigLayout(null, "[right, grow]"));
+
+        buttonBar.add(vypocitatButton);
+        return buttonBar;
 
 }
+    }
